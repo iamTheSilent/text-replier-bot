275579a54d29c96559441b6c4ec996fd81bdde5e
unknown <amirthesilent@gmail.com>
2023-11-14 16:02:58 +0330

    second try

--- bot.py
+++ bot.py
@@ -6,47 +6,62 @@ from telegram import Update
 
 from telegram.chataction import ChatAction
 
-token = "UR TOKEN"
+token = "5914279686:AAHKD0-Autn8ZNbynpgjG3pZHQZpoBxiRus"
+message= {
+    "msg_start":"سلام {} {} \n به ربات پیامرسان خوش آمدید.",
+    "msg_help":"استفاده ازین ربات خیلی راحته \n کافیه کمی فکر کنی",
+    "msg_contactUs":'ارتباط با ما فقط توسط آیدی زیر است \n @Shololow',
+    "msg_bcs": 'این کامند صرفا برای یاد آوری اینه که نشون بده سازنده این ربات به سریال \n better call saul \n علاقه منده',
+    "msg_sum": 'مجموع اعداد داده شده برابر با : \n {}'
+
+
+
+}
 
 
 def start_handler(update: Update, context: CallbackContext):
-    import pdb; pdb.set_trace()
     chat_id = update.message.chat_id
     first_name = update.message.chat.first_name
     last_name = update.message.chat.last_name
     context.bot.send_chat_action(chat_id, ChatAction.TYPING)
-    update.message.reply_text("سلام {} {} \n به ربات پیامرسان خوش آمدید.".format(first_name, last_name))
+    update.message.reply_text(text="msg_start".format(first_name, last_name))
 
 def help_handler(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     context.bot.send_chat_action(chat_id, ChatAction.TYPING)
-    update.message.reply_text("استفاده ازین ربات خیلی راحته \n کافیه کمی فکر کنی")
+    update.message.reply_text(text="msg_help")
 
 
 def contactus_handler(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     context.bot.send_chat_action(chat_id, ChatAction.TYPING)
-    update.message.reply_text('ارتباط با ما فقط توسط آیدی زیر است \n @Shololow')
+    update.message.reply_text(text="msg_contactUs")
 
 
 def bcs_handler(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     context.bot.send_chat_action(chat_id, ChatAction.TYPING)
-    update.message.reply_text('این کامند صرفا برای یاد آوری اینه که نشون بده سازنده این ربات به سریال \n better call saul \n علاقه منده')
+    update.message.reply_text(text="msg_bcs")
 
 
-updater = Updater(token, use_context=True)
+def sum_handler(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    numbers = context.args
+    result = sum(int(i) for i in numbers)
+    context.bot.send_chat_action(chat_id, ChatAction.TYPING)
+    update.message.reply_text(text="msg_sum".format(result))
 
-start_command = CommandHandler("start", start_handler)
-help_command = CommandHandler('help', help_handler)
-contactus_command = CommandHandler('contactUs', contactus_handler)
-bcs_command = CommandHandler('bcs', bcs_handler)
+def main():
+    updater = Updater(token, use_context=True)
 
-updater.dispatcher.add_handler(start_command)
-updater.dispatcher.add_handler(help_command)
-updater.dispatcher.add_handler(contactus_command)
-updater.dispatcher.add_handler(bcs_command)
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler('help', help_handler))
+    updater.dispatcher.add_handler(CommandHandler('contactUs', contactus_handler))
+    updater.dispatcher.add_handler(CommandHandler('bcs', bcs_handler))
+    updater.dispatcher.add_handler(CommandHandler('sum', sum_handler))
 
-updater.start_polling()
+    updater.start_polling()
 
-updater.idle()
+    updater.idle()
+
+main()
