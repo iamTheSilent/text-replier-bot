f3ff8cac5e67136fdf2de7a5047ab76ec8cb95ff
unknown <amirthesilent@gmail.com>
2023-12-19 16:36:41 +0330

    0.5

--- bot.py
+++ bot.py
@@ -18,10 +18,14 @@ message= {
     "msg_contactUs":'ارتباط با ما فقط توسط آیدی زیر است \n @Shololow',
     "msg_bcs": 'این کامند صرفا برای یاد آوری اینه که نشون بده سازنده این ربات به سریال \n better call saul \n علاقه منده',
     "msg_sum": 'مجموع اعداد داده شده برابر با : \n {}',
-    "msg_main_menu":"شروع",
+    "msg_main_menu":"منو اصلی:",
     "msg_mokaleme_menu": "یکی از موارد زیر را انتخاب کنید",
     "msg_numid": "جسجتو با آیدی",
     "msg_random":"جستجوی رندوم",
+    "msg_nonesense": "یکیوانتخاب کن",
+    "bot_nonesense": "ته انبار",
+    "bot_photo": "میخوای یه عکس ببینی؟",
+    "bot_music":"یه موزیکمون نشه؟",
     "bot_search":"جسجتو مکالمه",
     "bot_profile":"پروفایل",
     "bot_contact":"انتقاد و پیشنهاد",
@@ -30,10 +34,25 @@ message= {
 
 }
 
+def send_music_handler(update: Update, context: CallbackContext):
+    Chat_id = update.message.chat_id
+    with open("UR AUDIO FILE LOCATION", "rb") as music:
+        context.bot.send_chat_action(Chat_id, ChatAction.UPLOAD_AUDIO)
+        context.bot.sendAudio(Chat_id, music, caption= "my fav music these days", timeout= 5000)
+
+def send_photo_handler(update: Update, context: CallbackContext):
+    Chat_id = update.message.chat_id
+    with open("UR PHOTO FILE LOCATION", "rb") as img:
+        context.bot.send_chat_action(Chat_id, ChatAction.UPLOAD_PHOTO)
+        context.bot.sendPhoto(Chat_id, img , caption= "my fav photo these days", timeout= 3000)
+
+    
+
 def main_menu_handler(update: Update, context: CallbackContext):
     buttons = [
         [message["bot_search"],message["bot_profile"]], 
         [message["bot_contact"]],
+        [message["bot_nonesense"]],
         [message["bot_return"]]
     ]
     update.message.reply_text(
@@ -52,6 +71,18 @@ def search_handler(update: Update, context: CallbackContext):
 
     )
 
+def nonesense_handler(update: Update, context:CallbackContext):
+    buttons = [
+        [message["bot_music"],
+         message["bot_photo"]]
+    ]
+    update.message.reply_text(
+        text=message["msg_nonesense"],
+        reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+    )
+        
+    
 
 
 
@@ -102,6 +133,12 @@ def main():
     updater.dispatcher.add_handler(CommandHandler("menu", main_menu_handler))
     updater.dispatcher.add_handler(MessageHandler(Filters.regex(message["bot_search"]), search_handler))
     updater.dispatcher.add_handler(MessageHandler(Filters.regex(message["bot_return"]), return_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.regex(message["bot_music"]), send_music_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.regex(message["bot_photo"]), send_photo_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.regex(message["bot_nonesense"]), nonesense_handler))
+
+
+
 
     updater.start_polling()
 
